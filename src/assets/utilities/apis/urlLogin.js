562922a0b814cbb531149ec7a5401e849673f616
lgodoya <luis.godoy@unah.hn>
2022-04-08 08:12:17 -0600

    validacion de formularios frontend, inicio de sesion, registro de usuario(ok)

---
+++ src/assets/utilities/apis/urlLogin.js
@@ -0,0 +1,20 @@
+
+export const baseUrlLogin = "http://localhost:4000/api/v1/user/login"
+export const baseUrlRegister = "http://localhost:4000/api/v1/user/register"
+export const baseUrlChangePassword = "http://localhost:4000/api/v1//user/change-password"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/assets/utilities/setUserSession.js
@@ -0,0 +1,5 @@
+
+export const setUserSession = (token, user) => {
+  sessionStorage.setItem('token', token);
+  sessionStorage.setItem('user', JSON.stringify(user));
+}
