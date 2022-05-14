df1be1f213c848c78f5d559b38b052cabf8344c4
lgodoya <luis.godoy@unah.hn>
2022-05-14 08:01:23 -0600

    actualizado home

--- src/components/Views/Home/Home.js
+++ src/components/Views/Home/Home.js
@@ -1,16 +1,25 @@
-import React from 'react'
-import { } from '../../../assets/css/home/Home.css'
+import React, { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import SideBar from "../../../components/Sidebar/Sidebar";
+import Content from "../../Content/Content";
+import "../../../assets/css/home/Home.css";
+
 
 
 export const Home = () => {
+    const [sidebarIsOpen, setSidebarOpen] = useState(true);
+    const toggleSidebar = () => setSidebarOpen(!sidebarIsOpen);
+
+
     return (
-        <>
-            <nav className="navbar-h">
-                <p>vertical</p>
-            </nav>
-            <nav className="navbar-v flex-sm-column" >
-                <p>Horizontal</p>
-            </nav>
-        </>
+        <Router>
+            <div className="Home wrapper">
+                <SideBar toggle={toggleSidebar} isOpen={sidebarIsOpen} />
+                <Content toggleSidebar={toggleSidebar} sidebarIsOpen={sidebarIsOpen} />
+               
+            </div>
+        </Router>
     )
 }
