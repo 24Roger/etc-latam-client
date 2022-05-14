46a97989cb98e00e2fe3653b81183e205253b7ba
lgodoya <luis.godoy@unah.hn>
2022-05-14 08:50:33 -0600

    actualizada carpeta assets

--- src/assets/imgs/imagenes.js
+++ src/assets/imgs/imagenes.js
@@ -1,12 +1,14 @@
 import imglogin from './imglogin.png'
 import mesaayuda from './Mesa-Ayuda.png'
 import support from './support.png'
+import logoetc from './logo-etc.png'
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     "img1": imglogin,
     "img2": mesaayuda,
-    "img3": support
+    "img3": support,
+    "img4": logoetc
 
 }
--- src/assets/utilities/setUserSession.js
+++ src/assets/utilities/setUserSession.js
@@ -2,4 +2,8 @@
 export const setUserSession = (token, user) => {
   sessionStorage.setItem('token', token);
   sessionStorage.setItem('user', JSON.stringify(user));
+  
 }
+
+
+
